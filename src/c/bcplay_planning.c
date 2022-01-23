7331828905bfa48c473d126e689e362a03efbfdd
Rafael Lorandi <coolparadox@gmail.com>
2022-01-23 20:39:34 +0000

    enter character selection

--- src/c/bcplay_planning.c
+++ src/c/bcplay_planning.c
@@ -9,9 +9,13 @@
 #define BC_MODULE "planning"
 
 extern enum bc_planning_states _bc_planning_state;
+extern int _bc_planning_heroes_select;
+extern int _bc_planning_gameplay_verify;
 
 void bc_planning_init() {
     _bc_planning_state = BC_STATE_START;
+    _bc_planning_heroes_select = 0;
+    _bc_planning_gameplay_verify = 0;
 }
 
 enum bc_planning_states bc_planning_get_state() {
@@ -47,6 +51,7 @@ int _bc_planning_assess_appsite_wrong_network(const union bc_perception_detail* 
 
 int _bc_planning_assess_appsite_connect_wallet(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
     // App site awaits user for connecting to the wallet.
+    log_debug("advice: click button: connect wallet");
     struct bc_planning_hint* hint = advice->hints - 1;
     {
         (++hint)->type = BC_HINT_MOUSE_CLICK;
@@ -65,7 +70,13 @@ int _bc_planning_assess_appsite_connect_wallet(const union bc_perception_detail*
 
 int _bc_planning_assess_metamask_unlock(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
     // Metamask awaits for the unlock password.
+    log_debug("advice: type metamask password");
     struct bc_planning_hint* hint = advice->hints - 1;
+    {
+        (++hint)->type = BC_HINT_MOUSE_CLICK;
+        hint->detail.mouse_click.coord.col = 790;
+        hint->detail.mouse_click.coord.row = 340;
+    }
     (++hint)->type = BC_HINT_KEYBOARD_SEQUENCE; strcpy(hint->detail.keyboard_sequence.keys, "3hdna3Ut");
     (++hint)->type = BC_HINT_KEYBOARD_CLICK; strcpy(hint->detail.keyboard_click.key, "Return");
     advice->sleep = 10;
@@ -74,6 +85,7 @@ int _bc_planning_assess_metamask_unlock(const union bc_perception_detail* detail
 
 int _bc_planning_assess_metamask_signature_request(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
     // Metamask awaits for permission to accept game intention to connect to the wallet.
+    log_debug("advice: click button: sign");
     struct bc_planning_hint* hint = advice->hints - 1;
     {
         (++hint)->type = BC_HINT_MOUSE_CLICK;
@@ -92,6 +104,7 @@ int _bc_planning_assess_metamask_signature_request(const union bc_perception_det
 
 int _bc_planning_assess_game_kiosk_unscrolled(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
     // The game is inside the kiosk and the fullscreen button is not visible.
+    log_debug("advice: drag scroll bars: right, down");
     struct bc_planning_hint* hint = advice->hints - 1;
     {
         (++hint)->type = BC_HINT_MOUSE_DRAG;
@@ -113,6 +126,7 @@ int _bc_planning_assess_game_kiosk_unscrolled(const union bc_perception_detail* 
 
 int _bc_planning_assess_game_kiosk_scrolled(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
     // The game is inside the kiosk and the fullscreen button is visible.
+    log_debug("advice: click button: full screen");
     struct bc_planning_hint* hint = advice->hints - 1;
     {
         (++hint)->type = BC_HINT_MOUSE_CLICK;
@@ -126,6 +140,7 @@ int _bc_planning_assess_game_kiosk_scrolled(const union bc_perception_detail* de
 
 int _bc_planning_assess_socket_error(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
     // Seeing the dreadful 'socket error' message.
+    log_debug("advice: refresh url");
     struct bc_planning_hint* hint = advice->hints - 1;
     (++hint)->type = BC_HINT_KEYBOARD_CLICK; strcpy(hint->detail.keyboard_click.key, "F5");
     advice->sleep = 30;
@@ -134,6 +149,7 @@ int _bc_planning_assess_socket_error(const union bc_perception_detail* detail, s
 
 int _bc_planning_assess_game_selection(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
     // The game shows the selection of games.
+    log_debug("advice: click: treasure hunt");
     struct bc_planning_hint* hint = advice->hints - 1;
     {
         (++hint)->type = BC_HINT_MOUSE_CLICK;
@@ -141,23 +157,59 @@ int _bc_planning_assess_game_selection(const union bc_perception_detail* detail,
         hint->detail.mouse_click.coord.col = bc_random_sample_uniform(bbox->tl.col, bbox->br.col);
         hint->detail.mouse_click.coord.row = bc_random_sample_uniform(bbox->tl.row, bbox->br.row);
     }
-    advice->sleep = 2;
+    advice->sleep = 5;
     return 0;
 }
 
 int _bc_planning_assess_game_ongoing(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
-    // The game shows the selection of games.
-    log_warning("game ongoing: not implemented");
+    // The game is playing righ after selecting new heroes.
+    if (_bc_planning_gameplay_verify) {
+        log_debug("advice: let the game play a bit");
+        _bc_planning_gameplay_verify = 0;
+        // FIXME: debug
+        // advice->sleep = bc_random_sample_uniform(60 * 5, 60 * 15);
+        advice->sleep = 10;
+        return 0;
+    }
+    // The game is playing for quite some time.
+    log_debug("advice: click area: pause game");
+    _bc_planning_heroes_select = 1;
+    struct bc_planning_hint* hint = advice->hints - 1;
+    {
+        (++hint)->type = BC_HINT_MOUSE_CLICK;
+        hint->detail.mouse_click.coord.col = bc_random_sample_uniform(273, 686);
+        hint->detail.mouse_click.coord.row = bc_random_sample_uniform(480, 599);
+    }
+    advice->sleep = 2;
     return 0;
 }
 
 int _bc_planning_assess_game_paused(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
-    // The game shows the selection of games.
-    log_warning("game paused: not implemented");
+    // The game is paused.
+    struct bc_planning_hint* hint = advice->hints - 1;
+    if (_bc_planning_heroes_select) {
+        // The pause came from the normal gameplay.
+        _bc_planning_heroes_select = 0;
+        log_debug("advice: click button: heroes selection");
+        (++hint)->type = BC_HINT_MOUSE_CLICK;
+        const struct bc_bbox* bbox = &detail->game_paused.heroes;
+        hint->detail.mouse_click.coord.col = bc_random_sample_uniform(bbox->tl.col, bbox->br.col);
+        hint->detail.mouse_click.coord.row = bc_random_sample_uniform(bbox->tl.row, bbox->br.row);
+        advice->sleep = 3;
+        return 0;
+    }
+    // The pause came from the character selection screen.
+    log_debug("advice: click area: resume game play");
+    _bc_planning_gameplay_verify = 1;
+    (++hint)->type = BC_HINT_MOUSE_CLICK;
+    hint->detail.mouse_click.coord.col = bc_random_sample_uniform(40, 920);
+    hint->detail.mouse_click.coord.row = bc_random_sample_uniform(110, 480);
+    advice->sleep = 2;
     return 0;
 }
 
 int _bc_planning_assess_automatic_exit(const union bc_perception_detail* detail, struct bc_planning_recommendation* advice) {
+    log_debug("advice: refresh url");
     // The game shows the selection of games.
     struct bc_planning_hint* hint = advice->hints - 1;
     (++hint)->type = BC_HINT_KEYBOARD_CLICK; strcpy(hint->detail.keyboard_click.key, "F5");
@@ -184,7 +236,7 @@ int bc_planning_assess(const struct bc_perception* sight, struct bc_planning_rec
         case BC_GLIMPSE_SOCKET_ERROR: return _bc_planning_assess_socket_error(&sight->detail, advice);
         case BC_GLIMPSE_GAME_SELECTION: return _bc_planning_assess_game_selection(&sight->detail, advice);
         case BC_GLIMPSE_GAME_ONGOING: return _bc_planning_assess_game_ongoing(&sight->detail, advice);
-        case BC_GLIMPSE_GAME_PAUSED: return _bc_planning_assess_game_ongoing(&sight->detail, advice);
+        case BC_GLIMPSE_GAME_PAUSED: return _bc_planning_assess_game_paused(&sight->detail, advice);
         case BC_GLIMPSE_AUTOMATIC_EXIT: return _bc_planning_assess_automatic_exit(&sight->detail, advice);
     }
     panic("unknown glimpse: %d", sight->glimpse);
--- src/c/bcplay_planning_vars.c
+++ src/c/bcplay_planning_vars.c
@@ -1,4 +1,6 @@
 #include "bcplay_planning_types.h"
 
 enum bc_planning_states _bc_planning_state;
+int _bc_planning_heroes_select;
+int _bc_planning_gameplay_verify;
 
